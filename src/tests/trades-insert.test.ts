17b0102e752eadeb75eb14ab03b4bd7877064642
Roberto Seba <roberto@robertoseba.com>
2022-11-22 17:39:48 -0300

    insert tests

--- src/tests/trades-insert.test.ts
+++ src/tests/trades-insert.test.ts
@@ -5,8 +5,8 @@ import { Trade, priceHelper } from "../models/trades.schema";
 
 setupTestDb();
 
-describe("Trades Api - INSERT", () => {
-  describe("Insert VALID Trades - 201", () => {
+describe("TRADES API - INSERT", () => {
+  describe("INSERT VALID TRADES - 201", () => {
     const validData = {
       date: new Date(),
       ticker: "VALE5",
@@ -16,7 +16,7 @@ describe("Trades Api - INSERT", () => {
       fees: 0.01,
     };
 
-    test("Insert one valid trade", async () => {
+    test("INSERT ONE VALID TRADE", async () => {
       const res = await supertest(app)
         .post(`${API_START_URL}/trades`)
         .send(validData)
@@ -27,18 +27,19 @@ describe("Trades Api - INSERT", () => {
       expect(await Trade.count({})).toBe(1);
     });
 
-    test("Insert 3 valid trades same ticker and get new currAvgPrice and currTotalAmount", async () => {
+    test("INSERT 3 VALID TRADES SAME TICKER AND GET NEW CURRAVGPRICE AND CURRTOTALAMOUNT", async () => {
       let totalAmount = 0;
       let totalInvested = 0;
       for (let i = 1; i < 4; i++) {
-        validData.date = new Date();
-        validData.amount = validData.amount * i;
-        validData.price = validData.price * i;
-        totalInvested += validData.price * validData.amount;
-        totalAmount += validData.amount;
+        const newData = { ...validData };
+        newData.date = new Date();
+        newData.amount = newData.amount * i;
+        newData.price = newData.price * i;
+        totalInvested += newData.price * newData.amount;
+        totalAmount += newData.amount;
         const res = await supertest(app)
           .post(`${API_START_URL}/trades`)
-          .send(validData)
+          .send(newData)
           .expect("Content-Type", /json/)
           .expect(201);
         expect(res.body.currAvgPrice).toStrictEqual(
@@ -48,13 +49,85 @@ describe("Trades Api - INSERT", () => {
       }
       expect(await Trade.count({})).toBe(3);
     });
-    // TODO:
-    // Insert third trade to be older than second. Recalculate second inserted trade and make sure first is intact.
-    // Insert third trade to be older than first. Recalculate all trades.
+
+    test("INSERT TWO TRADES. THAN INSERT A NEW ONE IN BETWEEN.", async () => {
+      let totalAmount = 0;
+      let totalInvested = 0;
+      for (let i = 1; i < 3; i++) {
+        const newData = { ...validData };
+        newData.date = new Date(new Date().valueOf() + i * 24 * 60 * 60 * 1000);
+        newData.price = 100;
+        newData.amount = 100;
+        totalInvested += newData.price * newData.amount;
+        totalAmount += newData.amount;
+        const res = await supertest(app)
+          .post(`${API_START_URL}/trades`)
+          .send(newData)
+          .expect("Content-Type", /json/)
+          .expect(201);
+        expect(res.body.currAvgPrice).toStrictEqual(
+          priceHelper.setPrice(totalInvested / totalAmount)
+        );
+      }
+      const newInsertion = { ...validData };
+      newInsertion.date = new Date(
+        new Date().valueOf() + 1.5 * 24 * 60 * 60 * 1000
+      );
+      newInsertion.price = 250;
+      newInsertion.amount = 200;
+
+      await supertest(app)
+        .post(`${API_START_URL}/trades`)
+        .send(newInsertion)
+        .expect("Content-Type", /json/)
+        .expect(201);
+      expect(await Trade.count({})).toBe(3);
+      // checking all the records in DB
+      const records = await Trade.find({}).sort("date");
+      const avgPriceRecords = [100, 200, 175];
+      const amountRecords = [100, 300, 400];
+      records.forEach((record, idx) => {
+        expect(record.currAvgPrice).toStrictEqual(avgPriceRecords[idx]);
+        expect(record.currTotalAmount).toStrictEqual(amountRecords[idx]);
+      });
+    });
+
+    test("INSERT TWO TRADES. THAN INSERT A NEW ONE OLDER THAN THE TWO", async () => {
+      for (let i = 1; i < 3; i++) {
+        const newData = { ...validData };
+        newData.date = new Date();
+        newData.price = 100 / i;
+        newData.amount = 100;
+        const res = await supertest(app)
+          .post(`${API_START_URL}/trades`)
+          .send(newData)
+          .expect("Content-Type", /json/)
+          .expect(201);
+      }
+      const newInsertion = { ...validData };
+      newInsertion.date = new Date(new Date().valueOf() - 24 * 60 * 60 * 1000);
+      newInsertion.price = 50;
+      newInsertion.amount = 100;
+
+      await supertest(app)
+        .post(`${API_START_URL}/trades`)
+        .send(newInsertion)
+        .expect("Content-Type", /json/)
+        .expect(201);
+      expect(await Trade.count({})).toBe(3);
+      // checking all the records in DB
+      const records = await Trade.find({}).sort("date");
+      const avgPriceRecords = [50, 75, 66.67];
+      const amountRecords = [100, 200, 300];
+      records.forEach((record, idx) => {
+        expect(record.currAvgPrice).toStrictEqual(avgPriceRecords[idx]);
+        expect(record.currTotalAmount).toStrictEqual(amountRecords[idx]);
+      });
+    });
   });
 
-  describe("Insert INVALID Trades - 500", () => {
-    test("Insert one INVALID trade - WRONG DATE", async () => {
+  describe("INVALID TRADES - 500", () => {
+    test("WRONG DATE", async () => {
       const invalidData = {
         date: "20/20/01",
         ticker: "VALE5",
@@ -72,7 +145,7 @@ describe("Trades Api - INSERT", () => {
       expect(await Trade.count({})).toBe(0);
     });
 
-    test("Insert SAME trade TWICE", async () => {
+    test("SAME TRADE TWICE", async () => {
       const invalidData = {
         date: new Date(),
         ticker: "VALE5",
@@ -98,7 +171,7 @@ describe("Trades Api - INSERT", () => {
       expect(await Trade.count({})).toBe(1);
     });
 
-    test("Insert one INVALID trade - WRONG TICKER and WRONG DATE", async () => {
+    test("WRONG TICKER AND WRONG DATE", async () => {
       const invalidData = {
         date: "20/20/01",
         ticker: "xyz",
@@ -120,7 +193,26 @@ describe("Trades Api - INSERT", () => {
       });
       expect(await Trade.count({})).toBe(0);
     });
-    // TODO:
-    // try to sell more than in custody
+
+    test("SELL MORE THAN IN CUSTODY", async () => {
+      const invalidData = {
+        date: new Date(),
+        ticker: "Petr4",
+        price: 20,
+        amount: -200,
+        userEmail: "test@test.com",
+        fees: 0.01,
+      };
+      const res = await supertest(app)
+        .post(`${API_START_URL}/trades`)
+        .send(invalidData)
+        .expect("Content-Type", /json/)
+        .expect(500);
+      expect(res.body).toStrictEqual({
+        error:
+          "Total amount can not be negative. Trying to sell more than in custody",
+      });
+      expect(await Trade.count({})).toBe(0);
+    });
   });
 });
