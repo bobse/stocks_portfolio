eb51181f3e5a5085f50eef88f57e0bd68ddf7065
Roberto Seba <roberto@robertoseba.com>
2022-12-06 11:27:32 -0300

    login screen and ico

--- frontend/src/routes/Login/Login.jsx
+++ frontend/src/routes/Login/Login.jsx
@@ -1,11 +1,12 @@
 import { Box, Center, Heading, Text, Image, VStack } from "@chakra-ui/react";
 import GoogleButton from "../../assets/google_login.png";
+import Logo from "../../assets/logo.png";
+
 import auth from "../../services/auth";
 
 export const Login = (props) => {
    return (
       <VStack
-         h={"99vh"}
          w={"100vw"}
          maxW={"400px"}
          m="auto"
@@ -13,14 +14,17 @@ export const Login = (props) => {
          px={4}
          pb={4}
          pt={10}
+         mt={8}
+         alignItems="flex-end"
       >
-         <Box pb={8}>
-            <Heading pb={4}>Please Login</Heading>
-            <Text>
-               To access your stock portfolio you must login with the following:
-            </Text>
+         <Box pb={8} w="full">
+            <Center alignItems={"flex-start"}>
+               <Image src={Logo} w="100px" mr={4} />
+               <Heading pb={4}>Stocks Portfolio Manager</Heading>
+            </Center>
+            <Text>To access your stock portfolio you must login:</Text>
          </Box>
-         <Box>
+         <Box w={"full"}>
             <Center>
                <Image
                   cursor="pointer"
