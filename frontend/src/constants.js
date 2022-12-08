620e09bb5c1d9e0b14e4252d37a5187bbf10a8b1
Roberto Seba <roberto@robertoseba.com>
2022-12-08 16:48:10 -0300

    correction cookie samesite to lax

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,10 +1,10 @@
 let HOST = "";
-if (process.env.NODE_ENV === "production") {
-   const PORT = process.env.PORT || "5000";
-   HOST = `http://localhost:${PORT}`;
-} else {
-   HOST = "http://localhost:5000";
-}
+// if (process.env.NODE_ENV === "production") {
+//    const PORT = process.env.PORT || "5000";
+//    HOST = `http://localhost:${PORT}`;
+// } else {
+//    HOST = "http://localhost:5000";
+// }
 
 export const APIHOST = `${HOST}/api/v1`;
 export const APIPORTFOLIO = `${APIHOST}/portfolio`;
